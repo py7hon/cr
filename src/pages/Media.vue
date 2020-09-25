cbf18143f10ba3dbaa9499f44541dfd8a33fb035
Iqbal Rifai <yukifag@pm.me>
2020-09-25 11:40:22 +0000

    fix player

--- src/pages/Media.vue
+++ src/pages/Media.vue
@@ -1,8 +1,6 @@
 <template>
   <div v-if="media">
-    <div v-else class="player embed-responsive embed-responsive-16by9">
       <iframe :src="'https://player-cr.vercel.app/?id='+media.media_id" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen" allow="autoplay; encrypted-media *"></iframe>
-    </div>
 
     <div class="content-box" v-if="media.media_id === mediaId">
       <div class="clearfix">
